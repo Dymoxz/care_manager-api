f4395cc5b00abdf747732bef3ae391b9871c7727
memmik <menno.emmerik@hotmail.com>
2025-01-13 13:50:58 +0100

    created a controller for ClinicalProfile

---
+++ src/clinicalProfile/clinicalProfile.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { ClinicalProfileService } from './clinicalProfile.service';
+import { ClinicalProfile } from './clinicalProfile.schema';
+
+@Controller('clinicalProfile')
+export class clinicalProfileController {
+  constructor(private readonly clinicalProfileService: ClinicalProfileService) {}
+
+  @Get()
+  async getAll(): Promise<ClinicalProfile[]> {
+    return this.clinicalProfileService.getAll();
+  }
+}
