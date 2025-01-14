fa7915f7bf01f30c2e52cff026032d2892e464b3
Mech <stefrensma@gmail.com>
2025-01-14 16:21:03 +0100

    default values for caretaker and for isQuarantined

--- src/patient/patient.schema.ts
+++ src/patient/patient.schema.ts
@@ -7,51 +7,49 @@ import { Medicine } from '../medicine/medicine.schema';
 
 export type PatientDocument = HydratedDocument<Patient>;
 
-@Schema({timestamps: true})
+@Schema({ timestamps: true })
 export class Patient {
-    @Prop({required: true, unique: true})
-    patientNumber: number
+  @Prop({ required: true, unique: true })
+  patientNumber: number;
 
-    @Prop()
-    bsn: string;
+  @Prop()
+  bsn: string;
 
-    @Prop()
-    firstName: string;
+  @Prop()
+  firstName: string;
 
-    @Prop()
-    lastName: string;
+  @Prop()
+  lastName: string;
 
-    @Prop()
-    dateOfBirth: Date;
+  @Prop()
+  dateOfBirth: Date;
 
-    @Prop()
-    length: number;
+  @Prop()
+  length: number;
 
-    @Prop()
-    weight: number;
+  @Prop()
+  weight: number;
 
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'ClinicalProfile' })
-    clinicalProfile: ClinicalProfile;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'ClinicalProfile' })
+  clinicalProfile: ClinicalProfile;
 
-    @Prop()
-    diet: string;
+  @Prop()
+  diet: string;
 
-    @Prop()
-    careTaker: string;
+  @Prop({ default: null })
+  careTaker: string;
 
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Room' })
-    room: Room;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Room' })
+  room: Room;
 
+  @Prop({ type: [Agreement], default: [] }) // Embedded Agreement schemas
+  agreements: Agreement[];
 
+  @Prop({ default: false })
+  isQuarantined: boolean;
 
-    @Prop({ type: [Agreement], default: [] }) // Embedded Agreement schemas
-    agreements: Agreement[]
-
-    @Prop()
-    isQuarantined: boolean
-
-    @Prop({ type: [String], default: [] })
-    medicineAtcCodes: string[];
+  @Prop({ type: [String], default: [] })
+  medicineAtcCodes: string[];
 }
 
 export const PatientSchema = SchemaFactory.createForClass(Patient);
